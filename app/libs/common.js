a866a7a35a7af1294bb7852616f98c76d145abca
Oleg <frontendercode@gmail.com>
2019-07-21 14:39:25 +0300

    v.0.17
    Добавлены сниппеты галереи с изображениями, готовая к посадке на wordpress

--- app/libs/common.js
+++ app/libs/common.js
@@ -62,9 +62,85 @@ $.each(groupsOne, function() {
 
 
 
+
+var groupsthree = {};
+$('.gwt_content li:last-child a').each(function() {
+  var id = parseInt($(this).attr('data-group'), 10);
+  if(!groupsthree[id]) {
+    groupsthree[id] = [];
+  }
+  groupsthree[id].push( this );
+});
+$.each(groupsthree, function() {
+  $(this).magnificPopup({
+      type: 'image',
+      closeOnContentClick: true,
+       tClose: 'Закрыть(Esc)',
+    fixedContentPos: false,
+  fixedBgPos:false,
+      closeBtnInside: true,
+          closeMarkup: '<button title="%title%" class="mfp-close" style="position: absolute; top: 35px; right: 5px"><img src="/img/shut-down.png" width="24" height="24"/></button>',
+      gallery: { enabled:true,
+   tPrev: 'Вперед (левая стрелка key)', // Alt text on left arrow
+    tNext: 'Назад (правая стрелка key)', // Alt text on right arrow
+    tCounter: '%curr% из %total%', // Markup for "1 of 7" counter
+ // arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow custom-mfp-arrow-%dir%"><i class="fa fa-angle-%dir% fa-4x"></i></button>',
+       }
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var msnry = new Masonry( '.grid_masonry', {
   // options
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
