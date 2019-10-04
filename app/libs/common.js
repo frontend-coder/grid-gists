c2a8a980b6427a555fd104652874e756bdd8dbf7
Oleg <frontendercode@gmail.com>
2019-10-04 23:08:29 +0300

    v.0.22
    Изменены настройки Gulp

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
-    //  $("body").niceScroll({
-    // horizrailenabled:false
-    // });
-    // вверхнее красиво-вращающееся меню
-    // 1 и 2 строка это анимация крестика
-    //3 строка - слайдер вниз меню
-    //слайдер вниз меню отвечает за работу мобильного меню к переносу
-    $(".toggle-mnu").click(function() {
-        $(this).toggleClass("on");
-        $(".top-menu").slideToggle();
-        return false;
-    });
-    $('body, .top-menu ul li a').click(function() {
-        $('.hidden-mnu').hide("slow");
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     var groups = {};
@@ -77,7 +91,7 @@ $(document).ready(function() {
             fixedContentPos: false,
             fixedBgPos: false,
             closeBtnInside: true,
-            closeMarkup: '<button title="%title%" class="mfp-close" style="position: absolute; top: 35px; right: 5px"><img src="/img/shut-down.png" width="24" height="24"/></button>',
+            closeMarkup: '<button title="%title%" class="mfp-close" style="position: absolute; top: 35px; right: 5px"><img src="../img/shut-down.png" width="24" height="24"/></button>',
             gallery: {
                 enabled: true,
                 tPrev: 'Вперед (левая стрелка key)', // Alt text on left arrow
@@ -89,20 +103,20 @@ $(document).ready(function() {
     });
 
 
-    var msnry = new Masonry('.grid_masonry', {
-        //  columnWidth: '.grid-sizer',
-        // gutter: 10,
-        // columnWidth: '.grid-sizer_lazy',
-        // // options
-    });
+    // var msnry = new Masonry('.grid_masonry', {
+    //     //  columnWidth: '.grid-sizer',
+    //     // gutter: 10,
+    //     // columnWidth: '.grid-sizer_lazy',
+    //     // // options
+    // });
 
 
-    var msnrynew = new Masonry('.grid_masonry_new', {
-        //columnWidth: '.grid_masonry_new_w',
-        gutter: 10,
-        // columnWidth: '.grid-sizer_lazy',
-        // // options
-    });
+    // var msnrynew = new Masonry('.grid_masonry_new', {
+    //     //columnWidth: '.grid_masonry_new_w',
+    //     gutter: 10,
+    //     // columnWidth: '.grid-sizer_lazy',
+    //     // // options
+    // });
 
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -98,12 +98,8 @@ gulp.task('styles', () => {
 gulp.task('scripts', done => {
   var jsFiles = [
   'app/libs/plagins/jquery/jquery.min.js',
-//'app/libs/plagins/nicescroll/jquery.nicescroll.min.js',
-//'app/libs/plagins/jquery.PageScroll2id/jquery.PageScroll2id.min.js',
 'app/libs/plagins/magnific-popup/jquery.magnific-popup.min.js',
-'app/libs/plagins/masonry/masonry.js',
-// 'app/libs/plagins/owlcarousel/owl.carousel.min.js',
-//'app/libs/plagins/slick/slick.min.js',
+//'app/libs/plagins/masonry/masonry.js',
 'app/libs/common.js'
 // Always at the end
 ];
